5ab34f56f5bdbf7fb255c8408ba6bc8556942ac9
Sean Harvell <sean.harvell@iamzen.ninja>
2020-02-01 19:46:10 -0500

    updates

--- about/pushit.sh
+++ about/pushit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-asciidoctorj -n index.adoc 
-#asciidoctor -a toc=macro index.adoc
+#asciidoctorj -n index.adoc 
+asciidoctor -a toc=macro index.adoc
 #echo '<script src="switcher.js" type="text/javascript"></script>' >> index.html
 # document.getElementById("cw").contentDocument.querySelector("body > img").src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
 
